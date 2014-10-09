fa07d330e81a6e428eac89e2c20f54b006c69ec7
Sebastian Blumenthal <blumenthal@locomotec.com>
2014-10-09 17:55:05 +0200

    Cleaned up start scripts.

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# pointers to the relveant modules (this has to be needed to be adopted for individual systms)
-#export UBX_MODULES=/opt/src/sandbox/microblx/microblx_ros_bridge
-#export FBX_MODULES=/opt/src/sandbox/brics_3d_function_blocks
-
-# set up some other environtment scripts
-#source $UBX_ROOT/env.sh
+# set up some environtment scripts
 source $FBX_MODULES/env.sh
 
-# Start the micoiblox system
+# Start the ubx system
 exec $UBX_ROOT/tools/ubx_launch -webif 8888 -c rsgbridgetest.usc
