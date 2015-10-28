e575e66651f4b2b3b571e0b94d0315c8dbf3b425
Sebastian Blumenthal <blumenthal@locomotec.com>
2015-10-28 19:43:14 +0100

    Further updates for the install script.

--- install.sh
+++ install.sh
@@ -18,19 +18,19 @@
 # -------
 # The core software library is the BRICS_3D perception and modeling library
 # which contains an implementation of the "Robot Scene Graph (RSG)".
-# The RSG is used a mechanism to represent and exchange world model data.
+# The RSG is used as a mechanism to represent and exchange world model data.
 #
 # An instance of an RSG is embedded into a "World Model Agent". It is written
 # in C++ and can be queries with its respective API. 
 #
-# A SHERPA World Model as deployed on a robot has on World Model Agent
+# A SHERPA World Model as deployed on a robot has one World Model Agent
 # plus a set of communication componetns like ZMQ or ROS. The components
-# are relaized with the Microbox (UBX) frame work that allows to represent
-# the appliation in a system coposition model (.utc file). In a nutshell
-# the following dependencies need t be installed:
+# are relalized with the Microblox (UBX) framework that allows to represent
+# the appliation in a system coposition model (.utc file). 
+# In a nutshell the following dependencies need to be satisfied:
 # 
-# * the BRICS_3D library with all its dependencis including
-#   Boost, eigen3, HDF5, Libvariant for JSON, graphviz for debugging (optional)
+# * the BRICS_3D library with all its dependencies including
+#   Boost, Eigen3, HDF5, Libvariant for JSON, graphviz for debugging (optional)
 # * the UBX framework
 # * the BRICS_3D integration into the UBX framework (brics_3d_function_blocks 
 #   and ubx_robotscenegraph)
@@ -82,8 +82,8 @@ mkdir build
 cd build
 cmake -DHDF5_BUILD_CPP_LIB=true -DHDF5_BUILD_HL_LIB=true -DBUILD_SHARED_LIBS=true ..
 # A note on the used flags: the C++ API and the High Level (HL) APIs are used. The latter
-# one allows to use HDF5 in kind of messaging mode as we do. Further more the CMake
-# scripts of BRICS_3D are serching for shared libraries so we have to active it in the 
+# one allows to use HDF5 in a kind of messaging mode as we do. Furthermore, the CMake
+# scripts of BRICS_3D are searching for shared libraries so we have to activate it in the 
 # build process.  
 make
 make install
@@ -109,12 +109,13 @@ mkdir build && cd build
 cmake -DEIGEN_INCLUDE_DIR=/usr/include/eigen3 -DUSE_HDF5=true -DHDF5_1_8_12_OR_HIGHER=true -DUSE_JSON=true ..
 # A note on the used flags: For some reason the CMake find script has problems to find Eigen3 thus we currently
 # have to provide the path as a flag. HDF5 and JSON support has to be enabled because it is not a default option.
-# Furthermore, as we are usin HDF5 1.8.13 we have to enable the HDF5_1_8_12_OR_HIGHER flag to account for 
+# Furthermore, as we are using HDF5 1.8.13 we have to enable the HDF5_1_8_12_OR_HIGHER flag to account for 
 # major changes in the HDF5 API.
 make
 cd ..
 export BRICS_3D_DIR=$PWD" >> ~/.bashrc 
-#The BRICS_3D_DIR environemtn variable is needed for the other (below) modules to find BRICS_3D properly.
+#The BRICS_3D_DIR environment variable is needed for the other (below) modules to find BRICS_3D properly.
+source ~/.bashrc .
 cd ..
 
 ####################### Microblox #########################
@@ -136,15 +137,54 @@ ${SUDO} ln -s /usr/local/bin/luajit /usr/local/bin/lua
 ${SUDO} ldconfig
 cd ..
 
-echo "## Compile and install UBX. ###"
+echo ""
+echo "### Compile and install UBX. ###"
 git clone https://github.com/UbxTeam/microblx.git
 cd microblx
 source env.sh 
 # This sourcing of env.sh is important and is often overlooked during manual compilation.
 # It does configure a lot of LUA variables to make the UBX system models work.
 make
+echo "export UBX_ROOT=$PWD" >> ~/.bashrc
+# The UBX_ROOT environment variable is needed for the other (below) modules to find UBX properly.
+echo "export UBX_MODULES=/usr/local/ubx" >> ~/.bashrc
+# The UBX_MODULES variable points to the place where the UBX types, blocks etc. are installed. 
+# It is used to load the types and modules at run-time.  we set it to the UBX default folder.
+source ~/.bashrc .
 cd ..
 
+################ Communication modules #########################
+
+######### BRICS_3D integration into the UBX framework ###########
+echo ""
+echo "### BRICS_3D integration into the UBX framework  ###"
+
+echo "brics_3d_function_blocks:"
+git clone https://github.com/blumenthal/brics_3d_function_blocks.git
+cd brics_3d_function_blocks
+mkdir build
+cd build
+cmake .. -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+make
+make install
+cd ..
+echo "export FBX_MODULES=$PWD" >> ~/.bashrc
+#The FBX_MODULES environment variable is needed for the other (below) modules to find the BRICS_3D function blocks and typs.
+source ~/.bashrc .
+cd ..
+
+echo "ubx_robotscenegraph"
+# Note we do not need neccisarily need to check it out because this script is (currently) contained
+# in ubx_robotscenegraph. Though for the sake of completness we repeat it here.
+git clone https://github.com/blumenthal/ubx_robotscenegraph
+cd ubx_robotscenegraph
+mkdir build
+cd build
+cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ ..
+make
+make install
+cd ..
+cd ..
 
 
 
