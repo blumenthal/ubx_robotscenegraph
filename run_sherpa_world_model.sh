3dbd70d22d3698d39d1f53bb510f7c7c5c25c1d7
Sebastian Blumenthal <blumenthal@locomotec.com>
2015-10-30 15:47:02 +0100

    Added system model without ROS bridge.

--- run_sherpa_world_model.sh
+++ run_sherpa_world_model.sh
@@ -4,4 +4,14 @@
 source $FBX_MODULES/env.sh
 
 # Start the ubx system
-exec $UBX_ROOT/tools/ubx_launch -webif 8888 -c examples/sherpa/sherpa_world_model.usc	
+if [ "$1" = "--no-ros" ]; then
+  # Version without ROS bridge
+  exec $UBX_ROOT/tools/ubx_launch -webif 8888 -c examples/sherpa/sherpa_world_model_no_ros.usc
+elif [ "$1" != "" ]; then
+  # Version with explicit system composition model
+	exec $UBX_ROOT/tools/ubx_launch -webif 8888 -c ${1}
+else
+  # Default (full-fledged) system.
+  exec $UBX_ROOT/tools/ubx_launch -webif 8888 -c examples/sherpa/sherpa_world_model.usc
+fi
+
