3373f89f967e113a66789a75b6d7c3ebed144d48
Sebastian Blumenthal <blumenthal@locomotec.com>
2015-11-12 15:33:06 +0100

    Added examples on how to perform queries with the JSON API.

---
+++ examples/json_api/get.py
@@ -0,0 +1,26 @@
+#   Request-reply client in Python
+#   Connects REQ socket to tcp://localhost:22422
+#   Sends JSON Request as specifies by a file and
+#   print the reply
+#
+import zmq
+import sys
+
+# Setup query
+if len(sys.argv) > 1:
+    fileName =  sys.argv[1]
+    with open (fileName, "r") as messagefile:
+      message=messagefile.read()
+else:
+    message="{}"
+
+#  Prepare our context and sockets
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:22422")
+   
+print("Sending query: %s " % (message))
+socket.send_string(message)
+result = socket.recv()
+print("Received reult: %s " % (result))
+
