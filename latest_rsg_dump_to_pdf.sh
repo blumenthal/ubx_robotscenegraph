f789e68c774d40b12bc4ac1eac762b0523aba145
Sebastian Blumenthal <blumenthal@locomotec.com>
2016-01-27 11:39:47 +0100

    Made latest_rsg_dump_to_pdf script even more convenient.

--- latest_rsg_dump_to_pdf.sh
+++ latest_rsg_dump_to_pdf.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #
-# This script coverts the lates rsg dump file into a pdf by using thr dot tool
+# This script coverts the lates rsg dump file into a pdf by using thr dot tool.
 #
 # Dependencies
 # ------------
 # * graphviz e.g. sudo apt-get isntall graphviz
+#
+# Usage
+# -----
+# Call this script simply without any parameters:
+# ./latest_rsg_dump_to_pdf.sh
+#
+# OR call it and show it immediatly in a pdf viewer (here evince):
+#
+# evince `(./latest_rsg_dump_to_pdf.sh)`
 # 
 #
 # Authors
@@ -12,6 +21,6 @@
 #  * Sebastian Blumenthal (blumenthal@locomotec.com)
 
 LATEST_FILE=`(ls rsg_dump* -t1 | head -n1)`
-echo "Input file = ${LATEST_FILE}"
 
 dot ${LATEST_FILE} -Tpdf -o ${LATEST_FILE}.pdf
+echo "${LATEST_FILE}.pdf"
