2b5d6fcdc21197aa16a6f4f8a65e97d7dbb6f254
Sebastian Blumenthal <blumenthal@locomotec.com>
2016-02-23 21:37:45 +0100

    Replaced HDF5 intra robot communicaiton with JSON.

--- examples/multi_robot/robot_2.sh
+++ examples/multi_robot/robot_2.sh
@@ -3,16 +3,22 @@
 # set up some environtment scripts
 source $FBX_MODULES/env.sh
 
+
 # configure ports
-# swap JSON 
-export SWM_LOCAL_JSON_IN_PORT=12912
-export SWM_LOCAL_JSON_OUT_PORT=12911
+# reassign JSON ports 
+export SWM_LOCAL_JSON_IN_PORT=12921
+export SWM_LOCAL_JSON_OUT_PORT=12922
 # individual query server port
 export SWM_LOCAL_JSON_QUERY_PORT=22423
-# also swap the HDF5 ports
+
+# swap the inter robot communication ports
 export SWM_LOCAL_OUT_PORT=11511
 export SWM_REMOTE_OUT_PORT=11411
 
+# activate osm filter
+export SWM_ENABLE_INPUT_FILTER=1
+
+
 # Start the ubx system
-#exec $UBX_ROOT/tools/ubx_launch -webif 8889 -c rsg_robot_1.usc
+exec $UBX_ROOT/tools/ubx_launch -webif 8889 -c rsg_robot_1.usc
 exec $UBX_ROOT/tools/ubx_launch -webif 8889 -c rsg_robot_2.usc	
