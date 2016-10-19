0cca145086737d4f9a76a45d42a7cec2dfef6ee3
Sebastian Blumenthal <blumenthal@locomotec.com>
2016-10-19 14:46:25 +0200

    Added optional and configurable lua file for graph constraints.

---
+++ examples/sherpa/constraints.lua
@@ -0,0 +1,3 @@
+print("Adding grapg constraints")
+wm:addNodeAttribute(rootId, "rsg:agent_policy", "send no PointClouds");
+wm:addNodeAttribute(rootId, "rsg:agent_policy", "send no Atoms from context osm");
