af286397eec3a9e5b0546404d4bc5540fb166dff
Sebastian Blumenthal <blumenthal@locomotec.com>
2016-11-10 09:34:37 +0100

    Imporved launch script. It is now interactive.

--- swm_launch.sh
+++ swm_launch.sh
@@ -5,17 +5,7 @@
 # console but still usefull for autstart scripts.
 #
 
-./run_sherpa_world_model.sh  <<-EOF
-
--- Set all relevant functions to be called here:
-start_wm()
-start_auto_sync()
-
-
--- Keeps the system alive by blocking the interacive console: 
---while(true) do io.stdin:read'*l' end
-while(true) do 
-  os.execute("sleep 1") -- ignores CTRL C, but puts no extra load
-end
-
-EOF
+cat swm_launch.sh - | ./run_sherpa_world_model.sh
+  
+s()
+scene_setup()
